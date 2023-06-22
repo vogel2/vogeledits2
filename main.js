b76a57c32d8b5dbe3bcd181907b3e86043f14062
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2023-06-22 04:27:23 +0300

    Adding light theme

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const menuToggler = document.querySelector('.navbar-toggler')
 const drawer = document.querySelector('.navbar-drawer')
 
 menuToggler.addEventListener('click', () => drawer.toggleAttribute('enabled'))
+
+const themeToggler = document.querySelector('.theme-toggler')
+const html = document.querySelector('html')
+
+themeToggler.addEventListener('click', () => html.classList.toggle('light-theme'))
