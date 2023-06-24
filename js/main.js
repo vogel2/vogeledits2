6da839aefa78b792daccc7cdd53b887379420923
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2023-06-24 03:24:21 +0300

    Removing useless JavaScript code

--- js/main.js
+++ js/main.js
@@ -1,60 +1,39 @@
 const menuTogglers = document.querySelectorAll('.navbar-toggler')
 const drawer = document.querySelector('.navbar-drawer')
 
+// Enables the navigation menu
 menuTogglers.forEach(toggler =>
     toggler.addEventListener('click', () => drawer.toggleAttribute('enabled')))
 
 const navbarLinks = document.querySelectorAll('.navbar-links')
 
+// Closes the drawer on link click event
 navbarLinks.forEach(link =>
     link.addEventListener('click', () => drawer.attributes.removeNamedItem('enabled')))
 
 // Fade-in animation: create the observer
-var observer = new IntersectionObserver((entries) => {
-    applyAnimation(entries);
-});
-
-function applyAnimation(entries) {
-    entries.forEach((entry) => {
+const observer = new IntersectionObserver((entries) =>
+    entries.forEach(function (entry) {
         entry.target.classList.toggle("slide-up", entry.isIntersecting);
         if (entry.intersectionRatio > 0) observer.unobserve(entry.target);
-    });
-}
-
-function stopAnimation(entries) {
-    entries.forEach((entry) => observer.unobserve(entry.target));
-}
+    })
+);
 
 // Observe the elements
-[
+const elements = [
     ...document.querySelectorAll(".hero-item"),
     ...document.querySelectorAll("#videos section h2"),
     ...document.querySelectorAll(".grid-item > div"),
     ...document.querySelectorAll("#get-in-touch > *"),
-].forEach((element) => {
-    observer.observe(element);
-});
+]
+
+elements.forEach((element) => observer.observe(element));
 
 // Blue line animation
-window.onload = function () {
-    const styleSheet = document.styleSheets[0];
-    const rule = styleSheet.cssRules[23];
-    setTimeout(() => rule.style.width = '100%'
-        , 1500);
-};
+const styleSheet = document.styleSheets[0];
+const rule = styleSheet.cssRules[23];
+setTimeout(() => rule.style.width = '100%', 1500);
 
-// const inputs = document.querySelectorAll("#get-in-touch form input")
-
-// inputs.forEach(input =>
-//     input.addEventListener('change', _ =>
-//         checkFormValidity()
-//     )
-// )
-
-// function checkFormValidity() {
-//     const form = document.querySelector("#get-in-touch form")
-//     const submitButton = document.querySelector("#get-in-touch button[type='submit']")
-//     if (!form.checkValidity())
-//         submitButton.setAttribute('disabled', true)
-//     submitButton.removeAttribute('disabled');
-// }
+// Disabling right click on hero GIF
+const video = document.querySelector('.hero-avatar video')
+video.addEventListener('contextmenu', e => e.preventDefault())
